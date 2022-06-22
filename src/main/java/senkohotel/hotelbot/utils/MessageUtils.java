d905e3f506121b8a7027ca827d74f77f73df3ded
Flux <flustixshinu@gmail.com>
2022-06-22 16:24:47 +0200

    add message utils

---
+++ src/main/java/senkohotel/hotelbot/utils/MessageUtils.java
@@ -0,0 +1,41 @@
+package senkohotel.hotelbot.utils;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import senkohotel.hotelbot.Main;
+
+public class MessageUtils {
+    public static void reply(MessageReceivedEvent msg, String content) {
+        msg.getMessage()
+                .reply(content)
+                .mentionRepliedUser(false)
+                .complete();
+    }
+
+    public static void reply(MessageReceivedEvent msg, EmbedBuilder embed) {
+        msg.getMessage()
+                .reply(new MessageBuilder().setEmbeds(embed.build()).build())
+                .mentionRepliedUser(false)
+                .complete();
+    }
+
+    public static void reply(MessageReceivedEvent msg, MessageBuilder message) {
+        msg.getMessage()
+                .reply(message.build())
+                .mentionRepliedUser(false)
+                .complete();
+    }
+
+    public static void send(String channelID, String content) {
+        Main.bot.getTextChannelById(channelID).sendMessage(content).complete();
+    }
+
+    public static void send(String channelID, MessageBuilder message) {
+        Main.bot.getTextChannelById(channelID).sendMessage(message.build()).complete();
+    }
+
+    public static void send(String channelID, EmbedBuilder embed) {
+        Main.bot.getTextChannelById(channelID).sendMessage(new MessageBuilder().setEmbeds(embed.build()).build()).complete();
+    }
+}
