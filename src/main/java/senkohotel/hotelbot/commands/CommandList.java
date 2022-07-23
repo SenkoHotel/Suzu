bf851406b74ef61a5cecee42692034a0d47d5a3c
Flux <flustixshinu@gmail.com>
2022-07-23 17:50:49 +0200

    automatic command loading

--- src/main/java/senkohotel/hotelbot/commands/CommandList.java
+++ src/main/java/senkohotel/hotelbot/commands/CommandList.java
@@ -1,18 +1,27 @@
 package senkohotel.hotelbot.commands;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.reflections.Reflections;
 import senkohotel.hotelbot.Main;
 import senkohotel.hotelbot.command.HelpCommand;
 
-import java.util.Arrays;
-import java.util.TreeMap;
+import java.util.*;
 
 public class CommandList {
 
     static TreeMap<String, Command> commands = new TreeMap<>();
 
     public static void initList() {
-        addCommand(new HelpCommand());
+        Reflections reflections = new Reflections("senkohotel");
+        Set<Class<? extends Command>> classes = reflections.getSubTypesOf(Command.class);
+        for (Class<? extends Command> cmd : classes) {
+            try {
+                addCommand(cmd.getConstructor().newInstance());
+            } catch (Exception e) {
+                System.out.println("Couldn't add command " + cmd.getName());
+                e.printStackTrace();
+            }
+        }
     }
 
     static void addCommand (Command cmd) {
@@ -20,6 +29,7 @@ public class CommandList {
             return;
 
         commands.put(cmd.name, cmd);
+        System.out.println("Added command " + cmd.name);
     }
 
     public static void check(MessageReceivedEvent msg, String prefix) {
