aeabaeffcd2d36ea49e57f4d458aba7747e85ba2
Flux <flustixshinu@gmail.com>
2022-08-13 14:19:02 +0200

    remove useless returns

--- src/main/java/senkohotel/suzu/util/DBUtils.java
+++ src/main/java/senkohotel/suzu/util/DBUtils.java
@@ -13,16 +13,16 @@ public class DBUtils {
         return SuzuDB.execQuery("SELECT * FROM `ranks` ORDER BY `xp` DESC");
     }
 
-    public static ResultSet updateXP(int xp, String id) {
-        return SuzuDB.execQuery("UPDATE `ranks` SET `xp` = '" + xp + "' WHERE `ranks`.`userid` = '" + id + "'");
+    public static void updateXP(int xp, String id) {
+        SuzuDB.execQuery("UPDATE `ranks` SET `xp` = '" + xp + "' WHERE `ranks`.`userid` = '" + id + "'");
     }
 
-    public static ResultSet updateUsername(String name, String id) {
-        return SuzuDB.execQuery("UPDATE `ranks` SET `username` = '" + name + "' WHERE `ranks`.`userid` = '" + id + "'");
+    public static void updateUsername(String name, String id) {
+        SuzuDB.execQuery("UPDATE `ranks` SET `username` = '" + name + "' WHERE `ranks`.`userid` = '" + id + "'");
     }
 
-    public static ResultSet insertNewUser(int xp, String id, String name) {
-        return SuzuDB.execQuery("INSERT INTO `ranks` (`userid`, `xp`, `username`) VALUES ('" + id + "', '" + xp + "', '" + name + "')");
+    public static void insertNewUser(int xp, String id, String name) {
+        SuzuDB.execQuery("INSERT INTO `ranks` (`userid`, `xp`, `username`) VALUES ('" + id + "', '" + xp + "', '" + name + "')");
     }
 
     public static ResultSet getRoles() {
@@ -33,7 +33,7 @@ public class DBUtils {
         return SuzuDB.execQuery("SELECT * FROM `notifications` WHERE `uid` = '" + id + "'");
     }
 
-    public static ResultSet clearNotifications(String id) {
-        return SuzuDB.execQuery("DELETE FROM `notifications` WHERE `notifications`.`uid` = '" + id + "'");
+    public static void clearNotifications(String id) {
+        SuzuDB.execQuery("DELETE FROM `notifications` WHERE `notifications`.`uid` = '" + id + "'");
     }
 }
